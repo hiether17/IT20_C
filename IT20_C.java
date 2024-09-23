0290b44aa75002e020a39eb49cdc5c575d862ac2
hiether17 <lhietherfhiona@gmail.com>
2024-09-23 20:54:17 +0800

    added arrays

---
+++ IT20_C.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it20_c;
+
+/**
+ *
+ * @author HP
+ */
+public class IT20_C {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+        System.out.println("Hello love , Welcome to Data Structure and Algorithm");
+    }
+    
+}
